876dca7c963fbad4d04ebbe2e9c6ae6e1830b434
phoenix619 <phoenix619@gmail.com>
2015-05-05 02:18:16 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "UTD west campbell road!\n"+
+  "UTD west campbell road texas dalllas!\n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
   "User is #{ENV['ADMIN_USERNAME']}\n"+
