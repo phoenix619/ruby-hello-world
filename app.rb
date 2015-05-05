100e979e1e0ec5a938e20b6f902a50aee64f07ed
phoenix619 <phoenix619@gmail.com>
2015-05-05 03:20:02 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "KTLLKL SFEKFOEF FSDFSkjkjh klkl kkkl sdfsdf sfsdf sdf D SDSFSDF SFDSDFS DFSDF EFDSD\n"+
+  "I am here \n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
-  "User is #{ENV['ADMIN_USERNAME']}\n"+
+  "User is #{ENIV['ADMIN_USERNAME']}\n"+
   "Password is #{ENV['ADMIN_PASSWORD']}\n"+
   "DB password is #{ENV['DB_PASSWORD']}\n"
 end
