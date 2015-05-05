5b7d0832226f42d5a72d0c70b0503a94744b2082
phoenix619 <phoenix619@gmail.com>
2015-05-05 03:34:38 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "I am not here sfsdf sfsdfsdf \n"+
+  "Richardson Texas Here \n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
   "User is #{ENIV['ADMIN_USERNAME']}\n"+
